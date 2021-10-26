89274bab65cd631589ec880fffd1b0bbaf504b78
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-10-25 20:42:32 -0400

    created route for mysql to be expressed through server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,19 @@ const app = express();
 // Express Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'newageFORMAT19!*',
+        database: 'election'
+    },
+    console.log('Connected to the election database!')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
